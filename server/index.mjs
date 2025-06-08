8714153d622f9ce4e82c953c160e76ab4ffce9f6
vmoliterno <valeriamoliterno@outlook.com>
2025-06-08 20:43:07 +0200

    fix(models): aggiustate le classi per rispecchiare meglio le funzioni dao aggiunte

--- server/index.mjs
+++ server/index.mjs
@@ -1,9 +1,16 @@
 // imports
 import express from 'express';
+import initializeDatabase from "./src/data/seed.js"
 
 // init express
 const app = new express();
 const port = 3001;
+
+try {
+  await initializeDatabase();
+} catch {
+  console.error("Errore nell'inizializzazione del db");
+}
 
 // activate the server
 app.listen(port, () => {
